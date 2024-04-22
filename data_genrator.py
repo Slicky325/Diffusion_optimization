12dab8fdf9d2b0b33903b6146434c2a1ae3fc6fb
Atm1801 <103408821+Atm1801@users.noreply.github.com>
2024-04-22 17:06:32 +0530

    Add files via upload

--- data_genrator.py
+++ data_genrator.py
@@ -6,18 +6,10 @@ import torch
 import gc
 import numpy as np
 
-sample_prompts = [
-    "cute white mouse pilot wearing aviator glasses holding a flight helmet",
-    "A dignified beaver wearing glasses, a vest, and colorful necktie. He stands next to a tall stack of books in a library",
-    "Horses pulling a carriage on the moon's surface, with the Statue of Liberty and Great Pyramid in the background. Planet Earth can be seen in the sky.",
-    "A raccoon wearing formal clothes, wearing a tophat and holding a cane. The raccoon is holding a garbage bag. Oil painting in the style of Madhubani art.",
-    "A smiling sloth wearing a leather jacket, a cowboy hat, a kilt and a bowtie. The sloth is holding a quarterstaff and a big book. The sloth stands a few feet in front of a shiny VW van. The van has a cityscape painted on it and parked on grass.",
-    "A photo of a hamburger fighting a hot dog in a boxing ring. The hot dog is tired and up against the ropes.",
-    "A teddy bear wearing a motorcycle helmet and cape is car surfing on a taxi cab in New York City.",
-    "a photograph of Mona lisa drinking coffee as she has her breakfast. her plate has an omelet and croissant.",
-    "A bowl of soup that looks like a monster with tofu says deep learning.",
-    "A large yellow triangle above a green square and red rectangle"
-    ]
+with open('prompts.txt') as f:
+    sample_prompts = f.readlines()
+for i in range(len(sample_prompts)):
+    sample_prompts[i]=sample_prompts[i].rstrip("\n")
 
 seed = 0
 generator = torch.manual_seed(seed)
